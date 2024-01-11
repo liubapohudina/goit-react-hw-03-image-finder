1d848563ffb867a7a503619be2cd6f81cc8f4178
Liubov <drogaltseva92@gmail.com>
2024-01-11 16:24:50 +0100

    fix modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -24,8 +24,9 @@ export class Modal extends Component {
     
 
     handleClickElement = (event) => {
-        const currentClickItem = event.target;
-        if (currentClickItem) {
+      const currentClickItem = event.target.nodeName;
+      console.log(currentClickItem)
+        if (currentClickItem  === "DIV") {
             this.props.closeModal();
         }
     }
